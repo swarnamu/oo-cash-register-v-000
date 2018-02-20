a31b50fa801c9f406062420bddfa7138fbc7a164
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-02-20 20:42:36 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :discount, :total, :last_transaction, items
+  attr_accessor :discount, :total, :last_transaction, :items
 
   def initialize(discount = 0)
     @total = 0
